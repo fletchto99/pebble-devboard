b2a7519983074af51fe2186275a0da4faeacad5b
Matt Langlois <fletchto99@gmail.com>
2015-08-09 22:08:33 -0400

    Update devboard to use the Slate library

--- web/resources/js/settings.js
+++ web/resources/js/settings.js
@@ -1,73 +1,56 @@
-function getQueryParam(variable, default_) {
+function getQueryParam(variable, defaultValue) {
     var query = location.search.substring(1);
     var vars = query.split('&');
     for (var i = 0; i < vars.length; i++) {
         var pair = vars[i].split('=');
-        if (pair[0] == variable) {
+        if (pair[0] === variable) {
             return decodeURIComponent(pair[1]);
         }
     }
-    return default_ || false;
+    return defaultValue || '';
 }
 
-function saveOptions(username, password) {
-    return {
-        'username': username,
-        'password': password
-    };
-}
+$(function () {
 
-$(document).ready(function () {
-    var username = $('#username');
-    var password = $('#password');
-    var options = $('#options');
-    var progress = $('#progress');
-    var button_save = $('#save');
-    var button_cancel = $('#cancel');
+    var username = document.getElementById('username');
+    var password = document.getElementById('password');
+    var savebutton = document.getElementById('savebutton');
 
-    progress.hide();
-    button_cancel.click(function () {
-        document.location = getQueryParam('return_to', 'pebblejs://close#');
-    });
-    button_save.click(function () {
-        if (username.val() == '' || password.val() == '') {
+    savebutton.addEventListener('click', function() {
+
+        if (username.value == '' || password.value == '') {
             alert('Invalid username or password entered!');
             return;
         }
-        options.fadeToggle('1000', function () {
-            progress.fadeToggle('1000');
-        });
-
-
+        savebutton.value = 'Saving...';
+        savebutton.disabled = true;
         $.ajax({
             type: "POST",
             url: "api.php",
             dataType: "json",
             data: {
                 method: 'validatedeveloper',
-                username: username.val(),
-                password: password.val()
+                username: username.value,
+                password: password.value
             },
             success: function (data) {
                 if (data.status == 0) {
-                    progress.fadeToggle('1000');
-                    document.location = getQueryParam('return_to', 'pebblejs://close#') + encodeURIComponent(JSON.stringify(saveOptions(username.val(), password.val())));
+                    document.location = getQueryParam('return_to', 'pebblejs://close#') + encodeURIComponent(JSON.stringify({
+                            'username': username.value,
+                            'password': password.value
+                    }));
                 } else {
-                    progress.fadeToggle('1000', function () {
-                        options.fadeToggle('1000', function() {
-                            alert(data.message);
-                        });
-                    });
+                    savebutton.value = 'Save';
+                    savebutton.disabled = false;
+                    alert(data.message);
                 }
             },
-            error: function(data) {
-                progress.fadeToggle('1000', function () {
-                    options.fadeToggle('1000', function() {
-                        alert('An unknown error has occurred. Please close the settings and try again.');
-                    });
-                });
+            error: function (data) {
+                savebutton.value = 'Save';
+                savebutton.disabled = false;
+                alert('An unknown error has occurred. Please close the settings and try again.');
             }
         });
-
     });
+
 });
