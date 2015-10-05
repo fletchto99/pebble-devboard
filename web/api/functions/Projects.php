245d20efe1c820338d5f9aa08616cea4c66db9bf
Matt Langlois <fletchto99@gmail.com>
2015-10-04 21:58:51 -0400

    More efficiently determine which platforms the app is built for

--- web/api/functions/Projects.php
+++ web/api/functions/Projects.php
@@ -1,36 +1,39 @@
 <?php
 
-class Projects
-{
+class Projects {
 
-    function __construct($scraper, $url)
-    {
-        $this -> projects = Functions::retrieveProjects($scraper, $url);
+    function __construct($scraper, $url) {
+        $this->projects = Functions::retrieveProjects($scraper, $url);
     }
 
-    function execute()
-    {
+    function execute() {
         $projects = [];
-        if ($this -> projects['status'] == 0) {
-            $projs = Functions::findElements($this -> projects['projects'], '.application');
-            foreach($projs as $proj) {
+        if ($this->projects['status'] == 0) {
+            $projs = Functions::findElements($this->projects['projects'], '.application');
+            foreach ($projs as $proj) {
                 $project = [];
-                $project['title'] = Functions::elementToPlaintext(Functions::findElement($proj,'.application-title' , 0));
-                $releaseInfo = Functions::findElement($proj,'.application-release-info' , 0);
-                $project['version'] = Functions::elementToPlaintext(Functions::findElement($releaseInfo,'p' , 0));
-                $project['update_date'] = Functions::elementToPlaintext(Functions::findElement($releaseInfo,'p' , 1));
-                $icons = strtolower(Functions::elementToText(Functions::findElement($proj,'.application-icons' , 0)));
-                $project['aplite'] = (strpos($icons, 'aplite') !== false) ? 'true' : 'false';
-                $project['basalt'] = (strpos($icons, 'basalt') !== false) ? 'true' : 'false';
-                $project['timeline'] = (strpos($icons, 'timeline') !== false) ? 'Enabled' : 'Disabled';
-                $stats = Functions::findElement($proj,'.application-stats' , 0);
-                $project['hearts'] = intval(Functions::elementToPlaintext(Functions::findElement($stats,'p' , 0)));
-                $project['installs'] = intval(Functions::elementToPlaintext(Functions::findElement($stats,'p' , 1)));
+                $project['title'] = Functions::elementToPlaintext(Functions::findElement($proj, '.application-title', 0));
+                $releaseInfo = Functions::findElement($proj, '.application-release-info', 0);
+                $project['version'] = Functions::elementToPlaintext(Functions::findElement($releaseInfo, 'p', 0));
+                $project['update_date'] = Functions::elementToPlaintext(Functions::findElement($releaseInfo, 'p', 1));
+                $iconSection = strtolower(Functions::elementToText(Functions::findElement($proj, '.application-icons', 0)));
+                $icons = Functions::findElements($iconSection, 'img');
+                foreach ($icons as $icon) {
+                    $disabled = strpos($icon, 'hw-icon-disabled') > 0;
+                    $beginOffset = strpos($icon, 'hardware/') + 9;
+                    $length = strpos($icon, '-', $beginOffset) - $beginOffset;
+                    $item = substr($icon, $beginOffset, $length);
+                    $project[$item] = $disabled ? 'false' : 'true';
+                }
+                $stats = Functions::findElement($proj, '.application-stats', 0);
+                $project['hearts'] = intval(Functions::elementToPlaintext(Functions::findElement($stats, 'p', 0)));
+                $project['installs'] = intval(Functions::elementToPlaintext(Functions::findElement($stats, 'p', 1)));
                 array_push($projects, $project);
             }
         }
-        $this -> projects['projects'] = $projects;
-        return $this -> projects;
+        $this->projects['projects'] = $projects;
+
+        return $this->projects;
 
     }
 }
